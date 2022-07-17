a2ba0c83828959e3843e8e507ec923229dc6e8aa
Rob <robbielalderman@gmail.com>
2022-07-17 09:42:10 +0100

    half-ass navbar created. Links working, expanded on project descrptions, footer fixed, scale should be fixed, needs testing more post-deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,6 +48,11 @@ const linkStyle = {
   verticalAlign: "5%",
 }
 
+const captionStyle = {
+  color: "#666666",
+  fontSize: "12px",
+}
+
 const docLinkStyle = {
   ...linkStyle,
   listStyleType: "none",
@@ -58,12 +63,19 @@ const descriptionStyle = {
   color: "#232129",
   fontSize: "16px",
   marginTop: "30px",
+  width: "50%",
+  margin: "auto",
 }
 
 const docLink = {
   text: "Documentation",
   url: "https://www.gatsbyjs.com/docs/",
   color: "#8954A8",
+}
+
+const containerStyle = {
+  margin: "auto",
+  width: "60%",
 }
 
 // markup
@@ -77,14 +89,29 @@ export default function Home() {
     <Link to="/">&nbsp;r.&nbsp;</Link>
     </h1>
     <br />
-    
-  
-    
-
-    <div>
+    <div style={containerStyle}>
     <br />
-    <p style={descriptionStyle}>Scroll to see some of my work, feel free to get in touch, <br/>just let me know what you or your business is looking for</p>
-    <Link to="/contact/"><button style={{fontWeight: 500, letterSpacing: -1, textTransform: "uppercase",}}>
+    <br />
+    <p style={descriptionStyle}>
+      <Link class="navigation" to="https://vimeo.com/730701135">
+        Animation Link (Indicia)
+      </Link>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <Link class="navigation" to="https://github.com/RobbieLukeAlderman">
+        Github
+      </Link>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <Link class="navigation" to="https://www.behance.net/robbbb">
+        Behance
+      </Link>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <Link class="navigation" to="https://www.linkedin.com/in/robbbb/">
+        LinkedIn
+      </Link> </p>
+      <br />
+      <p style={descriptionStyle}>Scroll to see some of my work, feel free to get in touch, just let me know what you or your business is looking for!</p>
+      <br />
+    <Link to="/contact/"><button style={{marginTop: "25px", fontWeight: 500, letterSpacing: -1, textTransform: "uppercase",}}>
       work with me &nbsp;→
     </button></Link>
     <br />
@@ -92,13 +119,27 @@ export default function Home() {
       <img class="imgSize" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/85c30d93662449.5e6a46f20155d.png
 " style={{marginTop: "25px"}} />
     </Link>
-
     <br />
-
+  <p class="textAlign" style={captionStyle}>JAPANESE STREET-WEAR<br />
+WEB DESIGN AND DEVELOPMENT
+TOOLS USED : <br />
+[ ADOBE XD , ADOBE PHOTOSHOP , ATOM , GATSBY ]<br />
+[ JAVASCRIPT , REACTJS , HTML5 , CSS3 ]<br />
+[ <Link class="navigation" to="https://www.behance.net/gallery/93662449/GHST-WEB-DESIGN">LINK TO</Link> ] </p>
+    <br />
+    <br />
     <Link to="https://www.behance.net/gallery/72178557/SMNLT-Apparel-Design">
       <img class="imgSize" src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/1204b172178557.5bdf0267d45b4.png" />
     </Link>
-
+    <br />
+  <p class="textAlign" style={captionStyle}>SMNLT BRAND IDENTITY<br />
+MARKETING - DESIGN - PHOTOGRAPHY - PRINT
+KEY POINTS : <br />
+[ ADOBE ILLUSTRATOR , ADOBE PHOTOSHOP , LIGHTROOM ]<br />
+[ TRAVEL , LIVE , CREATE , PRODUCE ]<br />
+[ <Link class="navigation" to="https://www.behance.net/gallery/72178557/SMNLT-Apparel-Design">LINK TO</Link> ] </p>
+    <br />
+    <br />
     <br />
     <Link to="/"><button style={{fontWeight: 500, letterSpacing: -1, textTransform: "uppercase",}}>
     &nbsp; ↑ &nbsp;
